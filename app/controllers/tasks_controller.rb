5d0056a9f559e2605e27d577b5031b01088096ab
Analita <88949609+warlita@users.noreply.github.com>
2025-02-13 17:38:50 -0600

    rest of controllers and CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+
   def index
-        @tasks = Tasks.all
+        @tasks = Task.all # tasks are plural here bc index will show all of the tasks
   end
 
   def show
-    @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def set_task
+    @task = task.find(params[:id])
+  end
+
+    def task_params
+      params.expect(task: [ :name ])
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true
-  validates :description, allow_blank: true
+  validates :description, length: { maximum: 500 }, allow_blank: true # allow_blank is an option so it cannot be a stand alone validation - will get error, has to be tied to length
+
   validates :completed, inclusion: { in: [ true, false ] }
-  validates :pomodoro_count, only_integer: true
+  validates :pomodoro_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # this is to make sure the information is true when obtaining it from the user
 end
