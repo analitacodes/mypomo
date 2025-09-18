05a16c0bc1ca9edf1cf04447aaa24ce05e7c5545
Analita V. <88949609+analitacodes@users.noreply.github.com>
2025-09-17 19:49:53 -0500

    actually removing the test, I forgot the last time

--- test/controllers/pomodoro_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PomodoroControllerTest < ActionDispatch::IntegrationTest
-  test "should get start" do
-    get pomodoro_start_url
-    assert_response :success
-  end
-end
