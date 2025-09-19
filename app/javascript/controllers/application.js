3d288379b9a3be7032c1455a87736f8baf2dc2cc
Analita V. <88949609+analitacodes@users.noreply.github.com>
2025-09-18 19:35:10 -0500

    updated files to fix errors when running stimulus on local

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,12 @@
+import "@hotwired/turbo-rails"
+import "controllers"
+
 import { Application } from "@hotwired/stimulus"
-import "./controllers"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
-
-
-
